9823740e23ba53cf77e9375b2fe4d1495489477f
April <aprilrose.veloro@student.kyh>
2022-04-06 11:34:52 +0200

    Commit for the Test

--- Test/ReaderStuffTest.java
+++ Test/ReaderStuffTest.java
@@ -1,5 +1,29 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReaderStuffTest {
+
+    String expected = "fileforunittest.txt";
+    String currentWord;
+
+    @Test
+    void ReaderStuff() throws IOException {
+
+        FileReader fileReader = new FileReader(expected);
+        BufferedReader bufferedReader= new BufferedReader(fileReader);
+
+        currentWord = bufferedReader.readLine();
+        while (currentWord != null){
+            currentWord = bufferedReader.readLine();
+            System.out.println(currentWord);
+        }
+        //Assert
+        assertEquals(expected, "fileforunittest.txt");
+    }
 
 }
